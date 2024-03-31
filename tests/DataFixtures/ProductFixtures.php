c1aee882b74bfaf7382151bcd4367fb580d9c0f9
Mazzoni <mazzoni@gmx.fr>
2024-03-31 16:13:14 +0200

    Fixtures - Product

--- tests/DataFixtures/ProductFixtures.php
+++ tests/DataFixtures/ProductFixtures.php
@@ -8,11 +8,27 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
+    private const DEFAULT = 30;
+
     public function load(ObjectManager $manager): void
     {
-        $product = ProductBuilder::create()->build();
+        foreach ($this->generate() as $product) {
+            $manager->persist($product);
+        }
 
-        $manager->persist($product);
         $manager->flush();
+    }
+
+    private function generate(): iterable
+    {
+        for ($i = 1; $i <= self::DEFAULT; ++$i) {
+            yield ProductBuilder::create()
+                ->withName("Product {$i}")
+                ->withReference("Ref. {$i}")
+                ->withDescription(str_repeat('Lorem ipsum dolor sit amet.', 10))
+                ->withVat(0.20)
+                ->withPrice(50.00)
+                ->build();
+        }
     }
 }
